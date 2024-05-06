1a7f649cac432b52d3a413b7a2fad80190f26512
xiaxingli <156974915+xiaxingli@users.noreply.github.com>
2024-05-05 22:06:27 -0700

    Publish #1

---
+++ scripts.js
@@ -0,0 +1,336 @@
+
+
+
+
+
+/*
+var count = 0;
+             var button = document.getElementById("page_1_box_1");
+             var result = document.getElementById("result");
+             button.onclick = function() {
+                count++;
+                result.innerHTML = count;
+             }
+*/
+
+
+
+
+/*
+             function changeColor_1_1(event){
+                var color = '#000000';
+              event.style.backgroundColor=color;
+              document.getElementById('p4_1_1').style.color = '#000000';
+              }
+            
+              var count = 0;
+                     var button = document.getElementById("page_1_box_1");
+                     var result = document.getElementById("result");
+                     button.onclick = function() {
+                        count++;
+                        result.innerHTML = count;
+                     }
+*/
+            
+
+//PAGE 1 BOXES
+//PAGE 1 BOX 1     
+function changeColor_1_1(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_1_1').style.color='#EB6868';
+}
+
+
+//PAGE 1 BOX 2
+function changeColor_1_2(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_1_2').style.color='#EB6868';
+}
+     
+
+//PAGE 1 BOX 3
+function changeColor_1_3(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_1_3').style.color='#EB6868';
+}
+          
+
+//PAGE 1 BOX 4
+function changeColor_1_4(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_1_4').style.color='#EB6868';
+}
+            
+
+//PAGE 1 BOX 5
+function changeColor_1_5(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_1_5').style.color='#EB6868';
+}
+            
+
+//PAGE 1 BOX 6
+function changeColor_1_6(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_1_6').style.color='#EB6868';
+}
+            
+
+//PAGE 1 BOX 7
+function changeColor_1_7(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_1_7').style.color='#EB6868';
+}
+            
+
+//PAGE 1 BOX 8
+function changeColor_1_8(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_1_8').style.color='#EB6868';
+}
+            
+
+//PAGE 1 BOX 9
+function changeColor_1_9(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_1_9').style.color='#EB6868';
+}
+            
+
+//PAGE 1 BOX 10
+function changeColor_1_10(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_1_10').style.color='#EB6868';
+}
+            
+            
+            
+
+//PAGE 2 BOXES
+//PAGE 2 BOX 1     
+function changeColor_2_1(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_2_1').style.color='#EB6868';
+}
+
+
+//PAGE 2 BOX 2
+function changeColor_2_2(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_2_2').style.color='#EB6868';
+}
+     
+
+//PAGE 2 BOX 3
+function changeColor_2_3(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_2_3').style.color='#EB6868';
+}
+          
+
+//PAGE 2 BOX 4
+function changeColor_2_4(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_2_4').style.color='#EB6868';
+}
+            
+
+//PAGE 2 BOX 5
+function changeColor_2_5(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_2_5').style.color='#EB6868';
+}
+            
+
+//PAGE 2 BOX 6
+function changeColor_2_6(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_2_6').style.color='#EB6868';
+}
+            
+
+//PAGE 2 BOX 7
+function changeColor_2_7(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_2_7').style.color='#EB6868';
+}
+            
+
+//PAGE 2 BOX 8
+function changeColor_2_8(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_2_8').style.color='#EB6868';
+}
+            
+
+//PAGE 2 BOX 9
+function changeColor_2_9(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_2_9').style.color='#EB6868';
+}
+            
+
+//PAGE 2 BOX 10
+function changeColor_2_10(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_2_10').style.color='#EB6868';
+}
+            
+            
+            
+
+
+//PAGE 3 BOXES
+//PAGE 3 BOX 1     
+function changeColor_3_1(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_3_1').style.color='#EB6868';
+}
+
+
+//PAGE 3 BOX 2
+function changeColor_3_2(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_3_2').style.color='#EB6868';
+}
+     
+
+//PAGE 3 BOX 3
+function changeColor_3_3(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_3_3').style.color='#EB6868';
+}
+          
+
+//PAGE 3 BOX 4
+function changeColor_3_4(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_3_4').style.color='#EB6868';
+}
+            
+
+//PAGE 3 BOX 5
+function changeColor_3_5(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_3_5').style.color='#EB6868';
+}
+            
+
+//PAGE 3 BOX 6
+function changeColor_3_6(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_3_6').style.color='#EB6868';
+}
+            
+
+//PAGE 3 BOX 7
+function changeColor_3_7(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_3_7').style.color='#EB6868';
+}
+            
+
+//PAGE 3 BOX 8
+function changeColor_3_8(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_3_8').style.color='#EB6868';
+}
+            
+
+//PAGE 3 BOX 9
+function changeColor_3_9(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_3_9').style.color='#EB6868';
+}
+            
+
+//PAGE 3 BOX 10
+function changeColor_3_10(event){
+   var color = '#000000';
+   event.style.backgroundColor=color;
+   document.getElementById('p4_3_10').style.color='#EB6868';
+}
+            
+ 
+/*
+//PAGE 4
+//PAGE 4 BOX 1
+function changeColor_4_1(event){
+   var color = #D9D9D9;
+   event.style.backgroundColor=color;
+}
+*/
+
+/*
+document.querySelector('.PRINT_BUTTON').addEventListener('capture',function() {
+   html2canvas(document.querySelector('.result_page'),{
+      onrendered: function(canvas){
+         return Canvas2Image.saveAsPNG(canvas);
+     }
+     });
+   });
+   */
+   
+/*
+   function capture() {
+      html2canvas($('result_page'),{
+          onrendered: function (canvas) {                     
+              var imgString = canvas.toDataURL("image/png");
+              window.open(imgString);
+          }                  
+      }
+  });
+  */
+
+/*
+  document.querySelector('button').addEventListener('click', function() {
+   html2canvas(document.querySelector('.result_page'), {
+       onrendered: function(canvas) {
+           // document.body.appendChild(canvas);
+         return Canvas2Image.saveAsPNG(canvas);
+       }
+   });
+});
+*/
+
+const btn = document.querySelector('.takeImageBtn');
+
+btn.addEventListener('click',() => {
+   html2canvas(document.body)
+   .then(canvas => {
+      const url = canvas.toDataURL('image/png');
+      const a = document.createElement('a');
+      a.setAttribute('download','imageName.png');
+      a.setAttribute('href',url);
+      a.click();
+   })
+})
